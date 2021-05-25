37af3b0319d022fda79a5c78f1e0ea75d55a7053
Alexandre Santiago de Barros e Silva <alexandresantiagobs@outlook.com>
2021-05-25 20:30:31 -0300

    Add project files.

---
+++ src/NerdStore.Catologo.Domain/Dimesoes.cs
@@ -0,0 +1,16 @@
+﻿namespace NerdStore.Catologo.Domain
+{
+    public class Dimesoes
+    {
+        public decimal Altura { get; private set; }
+        public decimal Largura { get; private set; }
+        public decimal Profundidade { get; private set; }
+
+        public Dimesoes(decimal altura, decimal largura, decimal profundidade)
+        {
+            Altura = altura;
+            Largura = largura;
+            Profundidade = profundidade;
+        }
+    }
+}
