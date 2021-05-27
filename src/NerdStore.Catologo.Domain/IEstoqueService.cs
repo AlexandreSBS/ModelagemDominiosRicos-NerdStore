09204f004e130208048cfbe7dd4f467c641b8b81
Alexandre Santiago de Barros e Silva <alexandresantiagobs@outlook.com>
2021-05-26 23:16:21 -0300

    Criação do Serviço do Estoque

---
+++ src/NerdStore.Catologo.Domain/IEstoqueService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catologo.Domain
+{
+    public interface IEstoqueService : IDisposable
+    {
+        /// <summary>
+        /// Subtrai itens de um produto ao estoque.
+        /// </summary>
+        Task<bool> DebitarEstoque(Guid produtoId, int quantidade);
+        /// <summary>
+        /// Adiciona itens de um produto ao estoque.
+        /// </summary>
+        Task<bool> ReporEstoque(Guid produtoId, int quantidade);
+    }
+}
