79b4a50da9d8e7f0e4470b87db11022c0a96f606
Alexandre Santiago de Barros e Silva <alexandresantiagobs@gmail.com>
2021-05-29 18:30:34 -0300

    Adicionando migration de secutiry

--- src/NerdStore.WebApp.MVC/Data/ApplicationDbContext.cs
+++ src/NerdStore.WebApp.MVC/Data/ApplicationDbContext.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace NerdStore.WebApp.MVC.Data
 {
@@ -11,6 +9,23 @@ namespace NerdStore.WebApp.MVC.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            foreach (var entityType in builder.Model.GetEntityTypes())
+            {
+                var table = entityType.GetTableName();
+                if (table.StartsWith("AspNet"))
+                {
+                    entityType.SetTableName(table.Substring(6));
+                }
+
+                entityType.SetSchema("Security");
+            };
         }
     }
 }
